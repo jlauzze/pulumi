7064c9cc31f5d23074702d44b6667e5b84c34a4c
jlauzze <jlauzze@gmail.com>
2023-01-30 16:46:53 -0700

    my_first_app ready from Python perspetive.

--- my_first_app/__main__.py
+++ my_first_app/__main__.py
@@ -1,3 +1,13 @@
 """A Python Pulumi program"""
 
+import os
 import pulumi
+import pulumi_docker as docker
+
+stack = pulumi.get_stack()
+
+# Pull the backend image
+backend_image_name = "backend"
+backend = docker.RemoteImage(f"{backend_image_name}_image",
+                             name="pulumi/tutorial-pulumi-fundamentals-backend:latest"
+                            )
